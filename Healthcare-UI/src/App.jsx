83c96dad99e2704e71d8d080eca518596c1b05d4
omkar-git-hub-repository <omchavan9002@gmail.com>
2025-10-06 12:17:09 +0530

    updated routing between login and register

--- Healthcare-UI/src/App.jsx
+++ Healthcare-UI/src/App.jsx
@@ -1,7 +1,5 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import LoginForm from './components/login/LoginForm'
 import RegisterForm from './components/registration/RegisterForm'
@@ -10,20 +8,16 @@ function App() {
 
   const [isRegistering, setIsRegistering] = useState(false);
 
-
   return (
     <>
       <div>
-
         <Router>
           <Routes>
-            <Route path='/login' element={<LoginForm/>}> </Route>
-            <Route path='/register' element={<RegisterForm/>}> </Route>
+            <Route path="/" element={<LoginForm />} />
+            <Route path="/login" element={<LoginForm />} />
+            <Route path="/register" element={<RegisterForm />} />
           </Routes>
         </Router>
-
-
-
       </div>
     </>
   )
