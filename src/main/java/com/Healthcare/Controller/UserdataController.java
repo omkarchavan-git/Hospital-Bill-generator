92f16c7dbd506cd30175511b539af29d08cbe9c1
omkar-git-hub-repository <omchavan9002@gmail.com>
2025-10-07 10:30:10 +0530

    Implemented controller to add userdata

---
+++ src/main/java/com/Healthcare/Controller/UserdataController.java
@@ -0,0 +1,28 @@
+package com.Healthcare.Controller;
+
+import com.Healthcare.Entity.Userdata;
+import com.Healthcare.Service.UserdataService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/userdata")
+public class UserdataController {
+
+    @Autowired
+    private UserdataService userdataService;
+
+    @PostMapping("/registeruserdetails")
+    public ResponseEntity<Userdata> adduserdetails(@RequestBody Userdata userdata)
+    {
+       Userdata userdata1 = userdataService.adduserdata(userdata);
+       return new ResponseEntity<>(userdata1, HttpStatus.OK);
+    }
+
+}
