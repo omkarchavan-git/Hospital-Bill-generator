8dcd184aa25f5b2325382add309bff0f9775be70
omkar-git-hub-repository <omchavan9002@gmail.com>
2025-10-07 10:30:42 +0530

    Implemented method to add userdata

---
+++ src/main/java/com/Healthcare/ServiceImpl/UserdataServiceImpl.java
@@ -0,0 +1,21 @@
+package com.Healthcare.ServiceImpl;
+
+import com.Healthcare.Entity.Userdata;
+import com.Healthcare.Respository.UserdataRepo;
+import com.Healthcare.Service.UserdataService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserdataServiceImpl implements UserdataService {
+
+    @Autowired
+    private UserdataRepo userdataRepo;
+
+    //add user data
+    @Override
+    public Userdata adduserdata(Userdata userdata) {
+    Userdata user =   userdataRepo.save(userdata);
+        return user;
+    }
+}
