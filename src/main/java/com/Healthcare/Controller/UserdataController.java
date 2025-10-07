b349107fb648da687317b48e0662c6869d994efd
omkar-git-hub-repository <omchavan9002@gmail.com>
2025-10-07 10:38:24 +0530

    User registration data added successfully

--- src/main/java/com/Healthcare/Controller/UserdataController.java
+++ src/main/java/com/Healthcare/Controller/UserdataController.java
@@ -6,13 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/userdata")
+@CrossOrigin(origins = "http://localhost:5173")
 public class UserdataController {
 
     @Autowired
@@ -24,5 +22,4 @@ public class UserdataController {
        Userdata userdata1 = userdataService.adduserdata(userdata);
        return new ResponseEntity<>(userdata1, HttpStatus.OK);
     }
-
 }
