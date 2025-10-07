2e2d7c80f33b9eba9513684ca4a43b1cd2d57801
omkar-git-hub-repository <omchavan9002@gmail.com>
2025-10-07 12:25:06 +0530

    Implemented method to check valid user

--- src/main/java/com/Healthcare/Respository/UserdataRepo.java
+++ src/main/java/com/Healthcare/Respository/UserdataRepo.java
@@ -5,4 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserdataRepo extends JpaRepository<Userdata, Long> {
 
+    //method to find by username and pass
+    Userdata findByUsernameAndPassword(String username, String password);
 }
