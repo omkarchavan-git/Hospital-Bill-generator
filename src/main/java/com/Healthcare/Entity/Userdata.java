5e2207eaf49a0a64ba3dcf4dd7f083190cb114af
omkar-git-hub-repository <omchavan9002@gmail.com>
2025-10-09 21:57:55 +0530

    Added new entities

--- src/main/java/com/Healthcare/Entity/Userdata.java
+++ src/main/java/com/Healthcare/Entity/Userdata.java
@@ -18,11 +18,66 @@ public class Userdata {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
 
-    private String fullname;
+    private String firstname;
+    private String lastname;
     private String email;
-    private String username;
+    private long  mobile;
     private String password;
     private String confirmPassword;
 
+    public long getId() {
+        return id;
+    }
 
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public long getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(long mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
 }
